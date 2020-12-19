ce30911350b5aaebf03641ba595fbbcfafb53321
Jason Schroeder <jason.schroeder88@gmail.com>
2020-12-19 17:16:35 -0500

    Worked on login

--- src/main/webapp/js/Authenticator.js
+++ src/main/webapp/js/Authenticator.js
@@ -8,31 +8,40 @@ export default class Authenticator {
     }
 
     login() {
-        this.render();
+        async function login() {
+            let user = {
+                username:document.getElementById('username').value,
+                password:document.getElementById('password').value,
+            };
         
-        let user = {
-            username:document.getElementById('username').value,
-            password:document.getElementById('password').value,
-        };
-    
-        let response = await fetch(url + "login", {
-            method:"POST",
-            body:JSON.stringify(user),
-            credentials:'include'});
-    
-        if (response.status === 200) {
-            let validUser = await response.json();
-            userID = validUser[0];
-            userRole = validUser[1];
-        }
+            let response = await fetch(url + "login", {
+                method:"POST",
+                body:JSON.stringify(user),
+                credentials:'include'});
+        
+            if (response.status === 200) {
+                let validUser = await response.json();
+                userID = validUser[0];
+                userRole = validUser[1];
+            }
 
-        else {
-            document.getElementById('body').innerHTML='<h1>It Fails!';
+            else {
+                document.getElementById('body').innerHTML='<h1>It Fails!';
+            }
         }
     }
 
     logout() {
-
+        async function logout() {
+            let response = await fetch(url + "logout", {
+                method: "PUT",
+                credentials:'include'
+            });
+        
+            if (response === 200) {
+                return;
+            }
+        }
     }
     
     render() {

04c87ce5e5cfba70ec9f36b0452c8126e2011279
Jason Schroeder <jason.schroeder88@gmail.com>
2020-12-19 23:33:01 -0500

    Login and Logout working successfully

--- src/main/webapp/js/Authenticator.js
+++ src/main/webapp/js/Authenticator.js
@@ -1,39 +1,52 @@
 export default class Authenticator {
     constructor() {
         this.userType = '';
+        this.userId = '';
+    }
+
+    init() {
+        this.render();
+        
+        document.getElementById('loginButton').onclick = this.login;
     }
 
     getUserType() {
         return this.userType;
     }
 
-    login() {
-        async function login() {
-            let user = {
-                username:document.getElementById('username').value,
-                password:document.getElementById('password').value,
-            };
-        
-            let response = await fetch(url + "login", {
-                method:"POST",
-                body:JSON.stringify(user),
-                credentials:'include'});
-        
-            if (response.status === 200) {
-                let validUser = await response.json();
-                userID = validUser[0];
-                userRole = validUser[1];
-            }
+    getUserId() {
+        return this.userId;
+    }
 
-            else {
-                document.getElementById('body').innerHTML='<h1>It Fails!';
-            }
+    async login() {
+        let user = {
+            username:document.getElementById('username').value,
+            password:document.getElementById('password').value,
+        };
+    
+        let response = await fetch('http://localhost:8080/project-1/login', {
+            method:"POST",
+            body:JSON.stringify(user),
+            credentials:'include'});
+    
+        if (response.status === 200) {
+            let validUser = await response.json();
+            this.userId = validUser[0];
+            this.userType = validUser[1];
+            
+            return true;
+        }
+
+        else {
+            document.getElementById('appViewDiv').innerHTML='<h1>It Fails!</h1>';
+            return false;
         }
     }
 
     logout() {
         async function logout() {
-            let response = await fetch(url + "logout", {
+            let response = await fetch('http://localhost:8080/project-1/logout', 
+            {
                 method: "PUT",
                 credentials:'include'
             });
@@ -46,15 +59,19 @@ export default class Authenticator {
     
     render() {
         let title = document.getElementById('appTitle');
-        let body = document.getElementById('appBody');
+        let appView = document.getElementById('appViewDiv');
 
         title.innerText = "ERS Reimbursement System - Login";
 
-        let loginForm = document.createElement('div');
-        let loginFragment = document.createDocumentFragment();
-        
-        loginForm.appendChild(loginFragment);
-
-        body.appendChild(loginForm);
+        appView.innerHTML = `<h3>ERS Reimbursement System <br><br>Login</h3>
+        <input type="text" id="username" 
+            placeholder="Enter your username">
+        <br>
+        <br>
+        <input type="password" id="password" placeholder=
+            "Enter your password">
+        <br>
+        <br>
+        <button id='loginButton'>Login</button>`;     
     }
 }
--- src/main/webapp/js/ers.js
+++ src/main/webapp/js/ers.js
@@ -1,15 +1,121 @@
-import App from './App.js';
-import Authenticator from './Authenticator.js';
+const url = 'http://localhost:8080/project-1/';
+const appNavBar = document.getElementById('appNavBar');
+const welcomeNav = document.getElementById('welcomeNav');
+const viewPendingNav = document.getElementById('viewPendingNav');
+const addNav = document.getElementById('addNav');
+const viewPastNav = document.getElementById('viewPastNav');
+const logoutNav = document.getElementById('logoutNav');
+const appViewDiv = document.getElementById('appViewDiv');
 
-let authenticator = new Authenticator;
+let userID = 0;
+let userRole = 0;
 
-while (true) {
-    
-    authenticator.login();
-    
-    let app = new App(authenticator.getUserType());
+document.getElementById('loginButton').addEventListener('click', login);
+welcomeNav.addEventListener('click', goWelcome);
+logoutNav.onclick = logout;
 
-    app.render();
+async function login() {
+    let user = {
+        username:document.getElementById('username').value,
+        password:document.getElementById('password').value,
+    };
 
-    authenticator.logout();
+    let response = await fetch(url + "login", {
+        method:"POST",
+        body:JSON.stringify(user),
+        credentials:'include'});
+
+    if (response.status === 200) {
+        let validUser = await response.json();
+        userID = validUser[0];
+        userRole = validUser[1];
+
+        if (userRole === 1) {
+            appNavBar.setAttribute('style', 'display: block;');
+            welcomeNav.setAttribute('style', 'display: block;');
+            addNav.setAttribute('style', 'display: block;');
+            viewPastNav.setAttribute('style', 'display: block;');
+            logoutNav.setAttribute('style', 'display: block;')
+
+            goWelcome();
+        }
+        
+        else {
+            appNavBar.setAttribute('style', 'display: block;');
+            welcomeNav.setAttribute('style', 'display: block;');
+            viewPendingNav.setAttribute('style', 'display: block;');
+            logoutNav.setAttribute('style', 'display: block;')
+
+            goWelcome();
+        }
+    }
+
+    else {
+        document.getElementById('body').innerHTML='<h1>It Fails!';
+    }
 }
+
+async function logout() {
+    window.location.hash = "#login";
+    
+    appNavBar.setAttribute('style', 'display: none;');
+    welcomeNav.setAttribute('style', 'display: none;');
+    viewPendingNav.setAttribute('style', 'display: none;');
+    addNav.setAttribute('style', 'display: none;');
+    viewPastNav.setAttribute('style', 'display: none;');
+    logoutNav.setAttribute('style', 'display: none;');
+
+    appViewDiv.innerHTML = `<h3>ERS Reimbursement System <br><br>Login</h3>
+    <input type="text" id="username" 
+        placeholder="Enter your username">
+    <br>
+    <br>
+    <input type="password" id="password" placeholder=
+        "Enter your password">
+    <br>
+    <br>
+    <button id='loginButton'>Login</button>`;
+}
+
+function goWelcome() {
+    window.location.hash = '#welcome';
+    
+    if (userRole === 1) {
+        appViewDiv.innerHTML = "<h1>Is Employee!</h1>";
+    }
+
+    else {
+        appViewDiv.innerHTML = "<h1>Is Manager!</h1>";
+    }
+}
+
+// function getContent(fragmentId, callback){
+
+//     // lets do some custom content for each page of your website
+//     var pages = {
+//         home: "This is the Home page. Welcome to my site.",
+//         about: "This page will describe what my site is about",
+//         contact: "Contact me on this page if you have any questions"
+//     };
+
+//     // look up what fragment you are searching for in the object
+//     callback( pages[fragmentId]);
+// }
+
+// function loadContent(){
+
+//     var contentDiv = document.getElementById("app"),
+//     fragmentId = location.hash.substr(1);
+
+//     getContent(fragmentId, function (content) {
+//         contentDiv.innerHTML = content;
+//     });
+// }
+
+// if(!location.hash) {
+//     location.hash = "#home";
+// }
+
+// loadContent();
+
+// window.addEventListener("hashchange", loadContent)
